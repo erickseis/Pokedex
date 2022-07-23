da140a11ea54c79c4847913ab3f92f28f39b15ae
erickseis <erickseis@gmail.com>
2022-07-23 01:29:27 -0400

    primera cargas

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -28,7 +28,7 @@ const submit = (e) => {
     <div className="input-user">
         <form className='form-user' onSubmit={submit}>
             <h1>Hello!!! trainer...</h1>
-            <img className='imgUser img-characters' src="gh-pages/characters.png" alt="" />
+            <img className='imgUser img-characters' src="https://legends.pokemon.com/assets/home/characters.png" alt="" />
             <br />
             <input className='input-userName'
             type="text" 
@@ -40,8 +40,8 @@ const submit = (e) => {
             <button className='btn-user'><i class="fa-solid fa-paper-plane"></i></button>
         </form>
 <div>
-    <img className='imgUser mew-inputUser' id='mew' src="gh-pages/meww.png" alt="" />
-    <img className='imgUser mewtwo-inputUser' id='mewtwo' src="gh-pages/Mewtwo.png" alt="" />
+    <img className='imgUser mew-inputUser' id='mew' src="https://www.pngplay.com/wp-content/uploads/11/Mew-Pokemon-PNG-Photo-Image.png" alt="" />
+    <img className='imgUser mewtwo-inputUser' id='mewtwo' src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/905d008d-e774-4f04-8d1b-6aac3ef243c4/dalmygi-cb7f507e-5bb6-4aea-9751-9595b80c3b36.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzkwNWQwMDhkLWU3NzQtNGYwNC04ZDFiLTZhYWMzZWYyNDNjNFwvZGFsbXlnaS1jYjdmNTA3ZS01YmI2LTRhZWEtOTc1MS05NTk1YjgwYzNiMzYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.3suBV9pLhZkbQYqYrG8GvjFPlxj72HYfwdtGDpgGBm0" alt="" />
     <img className='imgUser pokeball-inputUser' id='transicion' src="gh-pages/pokeball-people-pokemon-nintendo-video-game-gaming-gartoon-ball-512.webp" alt="" />
 
 </div>
