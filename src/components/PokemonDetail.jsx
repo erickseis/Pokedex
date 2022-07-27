607437a534ac54cee389552054998dd10a735d14
erickseis <erickseis@gmail.com>
2022-07-26 21:51:14 -0400

    asd

--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -52,34 +52,34 @@ const PokemonDetail = () => {
             <section className='stats'>
                 <h1>STATS</h1>
                 <div className="progress hp">
-                    <div className="progress-bar" role="progressbar" aria-label="Example with label" style={{ width: `${character.stats?.[0].base_stat}%` }} aria-valuenow={`${character.stats?.[0].base_stat}`} aria-valuemin="0" aria-valuemax="100">  {character.stats?.[0].base_stat}% </div>
+                    <div className="progress-bar" role="progressbar" aria-label="Example with label" style={{ width: `${character.stats?.[0].base_stat}%` }} aria-valuenow={`${character.stats?.[0].base_stat}`} aria-valuemin="0" aria-valuemax="100">HP    {character.stats?.[0].base_stat}% </div>
                     
                 </div>
                 <br />
                 <div className="progress atack">
-                    <div className="progress-bar" role="progressbar" aria-label="Example with label" style={{ width: `${character.stats?.[1].base_stat}%` }} aria-valuenow={`${character.stats?.[1].base_stat}`} aria-valuemin="0" aria-valuemax="100">{character.stats?.[1].base_stat}%</div>
+                    <div className="progress-bar" role="progressbar" aria-label="Example with label" style={{ width: `${character.stats?.[1].base_stat}%` }} aria-valuenow={`${character.stats?.[1].base_stat}`} aria-valuemin="0" aria-valuemax="100"> ATACK {character.stats?.[1].base_stat}%</div>
                 </div>
                 <br />
                 <div className="progress defense">
-                    <div className="progress-bar" role="progressbar" aria-label="Example with label" style={{ width: `${character.stats?.[2].base_stat}%` }} aria-valuenow={`${character.stats?.[2].base_stat}`} aria-valuemin="0" aria-valuemax="100">{character.stats?.[2].base_stat}%</div>
+                    <div className="progress-bar" role="progressbar" aria-label="Example with label" style={{ width: `${character.stats?.[2].base_stat}%` }} aria-valuenow={`${character.stats?.[2].base_stat}`} aria-valuemin="0" aria-valuemax="100"> DEFENSE {character.stats?.[2].base_stat}%</div>
                 </div>
 
                 <br />
                 <div className="progress  Sp. Attack">
-                    <div className="progress-bar" role="progressbar" aria-label="Example with label" style={{ width: `${character.stats?.[3].base_stat}%` }} aria-valuenow={`${character.stats?.[3].base_stat}`} aria-valuemin="0" aria-valuemax="100">{character.stats?.[3].base_stat}%</div>
+                    <div className="progress-bar" role="progressbar" aria-label="Example with label" style={{ width: `${character.stats?.[3].base_stat}%` }} aria-valuenow={`${character.stats?.[3].base_stat}`} aria-valuemin="0" aria-valuemax="100">SP ATACK {character.stats?.[3].base_stat}%</div>
                 </div>
 
                 <br />
                 <div className="progress  Sp. Defense">
-                    <div className="progress-bar" role="progressbar" aria-label="Example with label" style={{ width: `${character.stats?.[4].base_stat}%` }} aria-valuenow={`${character.stats?.[4].base_stat}`} aria-valuemin="0" aria-valuemax="100">{character.stats?.[4].base_stat}%</div>
+                    <div className="progress-bar" role="progressbar" aria-label="Example with label" style={{ width: `${character.stats?.[4].base_stat}%` }} aria-valuenow={`${character.stats?.[4].base_stat}`} aria-valuemin="0" aria-valuemax="100"> SP DEFENSE {character.stats?.[4].base_stat}%</div>
                 </div>
                 <br />
-                <div className="progress  Sp. speed">
-                    <div className="progress-bar" role="progressbar" aria-label="Example with label" style={{ width: `${character.stats?.[5].base_stat}%` }} aria-valuenow={`${character.stats?.[5].base_stat}`} aria-valuemin="0" aria-valuemax="100">{character.stats?.[5].base_stat}%</div>
+                <div className="progress speed">
+                    <div className="progress-bar" role="progressbar" aria-label="Example with label" style={{ width: `${character.stats?.[5].base_stat}%` }} aria-valuenow={`${character.stats?.[5].base_stat}`} aria-valuemin="0" aria-valuemax="100"> SPEED {character.stats?.[5].base_stat}%</div>
                 </div>
-
-                <h2><strong>Total</strong>"suma total "</h2>
-
+                    <br />
+                <h2><strong>Total </strong>"suma total "</h2>
+            
             </section>
             <br />
             <section className='container-move'>
